0b9d079be6d59fa488dc3e1747593a7c716c875e
Gavin Hales <gavinahales@gmail.com>
2017-03-07 00:18:01 +0000

    Mote setup
    Some more mote setup code added
    setmote function added

--- moteservice.py
+++ moteservice.py
@@ -2,14 +2,36 @@ from flask import Flask, request, abort
 from mote import Mote
 
 app = Flask(__name__)
-#If a mote isn't connected the app falls over at this point.
-#mote = Mote()
+
+#Try connecting to the Mote, but make sure it fails gracefully.
+try:
+    mote = Mote()
+    mote.configure_channel(1, 16, False)
+    mote.configure_channel(2, 16, False)
+    mote.configure_channel(3, 16, False)
+    mote.configure_channel(4, 16, False)
+    mote.clear()
+except IOError:
+    mote = None
+
+
+def setmote(channels, red, green, blue):
+    mote.clear()
+    for channel in channels:
+        for pixel in range(0, 16):
+            mote.set_pixel(channel, pixel, red, green, blue)
+    mote.show()
+
 
 @app.route('/motestatus/', methods=['GET'])
 def lightstatusreq():
     if request.method == 'GET':
         #TO BE IMPLEMENTED
-        return 'This will return the current status of the lights.'
+        if mote is None:
+            return 'Mote device not connected.'
+        else:
+            #Can do this using mote.get_pixel()
+            return 'This will return the current status of the lights.'
     else:
         abort(405)
 
@@ -20,6 +42,14 @@ def setmotereq():
         return 'This will set the Mote lights'
     else:
         abort(405)
+
+@app.route('/connectmote/', methods=['GET, POST'])
+def connectmote():
+    try:
+        mote = Mote()
+    except IOError:
+        mote = None
+        return 'Mote device not found. Connect failed.'
 
 #USED FOR DEVELOPMENT ON WINDOWS ONLY. REMOVE WHEN COMPLETE
 if __name__ == '__main__':
